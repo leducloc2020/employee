df61fbc84fcdfa11f076dcc1c8e8019085bad84f
leducloc2020 <130266911+leducloc2020@users.noreply.github.com>
2024-04-20 15:35:00 +0000

    Updates

--- utils/error.js
+++
@@ -1,7 +0,0 @@
-export const errorHandler = (statusCode, userID, message) => {
-    const error = new Error();
-    error.statusCode = statusCode;
-    error.message = message;
-    error.userID = userID;
-    return error
-}
